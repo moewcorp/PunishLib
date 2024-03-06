b9dfb82d4aefbfb5a44dced763b66ca9b6f9e5f5
NiGuangOwO <m2589895500@gmail.com>
2024-03-06 23:27:48 +0800

    1

--- PunishLib/AboutPlugin.cs
+++ PunishLib/AboutPlugin.cs
@@ -14,6 +14,7 @@ namespace PunishLib
     {
         public string Sponsor { get; set; }
         public string Developer { get; set; }
+        public string Translator { get; set; }
 
         internal Action WithLoveBy => Developer == null ? WithLoveByPunish : WithLoveByOther;
 
@@ -29,6 +30,36 @@ namespace PunishLib
                 ImGui.SameLine(0, 0);
                 ImGuiEx.Text($" by Puni.sh");
             });
+            ImGuiEx.ImGuiLineCentered("AboutHeader2", delegate
+            {
+                ImGuiEx.Text($"本汉化版的本地化和功能适配，");
+                ImGui.PushFont(UiBuilder.IconFont);
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudRed, FontAwesomeIcon.Heart.ToIconString());
+                ImGui.PopFont();
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text($"来自{PunishLibMain.About.Translator} (猫猫公司)");
+            });
+            ImGuiEx.ImGuiLineCentered("AboutHeader3", delegate
+            {
+                ImGui.PushFont(UiBuilder.IconFont);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.PopFont();
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text("插件开源免费，汉化维护不易，请勿从任何闲鱼小店上购买本插件（包括但不限于售卖插件文件，倒卖信息差）");
+                ImGui.PushFont(UiBuilder.IconFont);
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.PopFont();
+            });
         }
 
         static void WithLoveByOther()
@@ -43,6 +74,36 @@ namespace PunishLib
                 ImGui.PopFont();
                 ImGui.SameLine(0, 0);
                 ImGuiEx.Text($" by {PunishLibMain.About.Developer}, published by Puni.sh");
+            });
+            ImGuiEx.ImGuiLineCentered("AboutHeader2", delegate
+            {
+                ImGuiEx.Text($"本汉化版的本地化和功能适配，");
+                ImGui.PushFont(UiBuilder.IconFont);
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudRed, FontAwesomeIcon.Heart.ToIconString());
+                ImGui.PopFont();
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text($"来自{PunishLibMain.About.Translator} (猫猫公司)");
+            });
+            ImGuiEx.ImGuiLineCentered("AboutHeader3", delegate
+            {
+                ImGui.PushFont(UiBuilder.IconFont);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.PopFont();
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text("插件开源免费，汉化维护不易，请勿从任何闲鱼小店上购买本插件（包括但不限于售卖插件文件，倒卖信息差）");
+                ImGui.PushFont(UiBuilder.IconFont);
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudYellow, FontAwesomeIcon.ExclamationTriangle.ToIconString());
+                ImGui.PopFont();
             });
         }
     }
